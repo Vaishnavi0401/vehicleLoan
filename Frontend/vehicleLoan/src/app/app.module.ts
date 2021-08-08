7a456d2daeb2e7bb0340b08a0808433925f90699
VaishnaviDaber04 <Vaishnavi.Daber@lntinfotech.com>
2021-08-09 00:12:15 +0530

    Committing Identity Details, Vehicle Details, Emi- Calculator and Admin Dashboard

--- Frontend/vehicleLoan/src/app/app.module.ts
+++ Frontend/vehicleLoan/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmiCalculatorComponent } from './emi-calculator/emi-calculator.component';
+import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
+import { IdentityDetailsComponent } from './identity-details/identity-details.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmiCalculatorComponent,
+    VehicleDetailsComponent,
+    IdentityDetailsComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
