1ec49c72766870622fc64b5ca3a2057fa91ac2b6
Jatin Acharya <52876123+JatinAcharya@users.noreply.github.com>
2021-08-08 22:17:56 +0530

    home,login aboutus, pagenotfound components added

--- Frontend/vehicleLoan/src/app/app-routing.module.ts
+++ Frontend/vehicleLoan/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent,
+  children:[
+    {path:'about-us', component:AboutUsComponent},
+    {path: 'login', component: LoginComponent},
+  ]
+},
+  // {path: '/about-us', component: AboutUsComponent},
+  // {path: '/login', component: LoginComponent},
+  // {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
