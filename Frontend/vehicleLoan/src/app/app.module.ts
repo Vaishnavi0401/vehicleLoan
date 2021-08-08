952c2201e51623c0dd68f30958305bef038b21b9
Krutika-Hiranandani <krutika.hiranandani@lntinfotech.com>
2021-08-08 23:51:00 +0530

    New Commit

--- Frontend/vehicleLoan/src/app/app.module.ts
+++ Frontend/vehicleLoan/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetEligibilityComponent } from './get-eligibility/get-eligibility.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { EmploymentDetailsComponent } from './employment-details/employment-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetEligibilityComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    EmploymentDetailsComponent
   ],
   imports: [
     BrowserModule,
