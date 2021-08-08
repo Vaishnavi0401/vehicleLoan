f2165e6a369e3d2fdac06dbcba53f0db91fa90bb
VineetRathish <88103842+VineetRathish@users.noreply.github.com>
2021-08-09 00:19:36 +0530

    adding personal details, loan offer, loan details, dashboard
    adding personal details, loan offer, loan details, dashboard

--- Frontend/vehicleLoan/src/app/app.module.ts
+++ Frontend/vehicleLoan/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { LoanOfferComponent } from './loan-offer/loan-offer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoanDetailsComponent } from './loan-details/loan-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent,
+    LoanOfferComponent,
+    DashboardComponent,
+    LoanDetailsComponent
   ],
   imports: [
     BrowserModule,
