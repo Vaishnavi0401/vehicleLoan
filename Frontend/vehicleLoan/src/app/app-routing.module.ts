a63165fdb0858c7b6fc40182bea4458ddaf6c5f2
Jatin Acharya <52876123+JatinAcharya@users.noreply.github.com>
2021-08-10 22:03:46 +0530

    routing added

--- Frontend/vehicleLoan/src/app/app-routing.module.ts
+++ Frontend/vehicleLoan/src/app/app-routing.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { EmiCalculatorComponent } from './emi-calculator/emi-calculator.component';
+import { EmploymentDetailsComponent } from './employment-details/employment-details.component';
+import { GetEligibilityComponent } from './get-eligibility/get-eligibility.component';
 import { HomeComponent } from './home/home.component';
+import { IdentityDetailsComponent } from './identity-details/identity-details.component';
+import { LoanDetailsComponent } from './loan-details/loan-details.component';
+import { LoanOfferComponent } from './loan-offer/loan-offer.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent,
   children:[
     {path:'about-us', component:AboutUsComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'emi-calculator', component: EmiCalculatorComponent},
+    {path: 'check-eligibility', component: GetEligibilityComponent},
+    {path: 'loan-offers', component: LoanOfferComponent},
+    {path: 'personal-details', component: PersonalDetailsComponent},
+    {path: 'employment-details', component: EmploymentDetailsComponent},
+    {path: 'vehicle-details', component: VehicleDetailsComponent},
+    {path: 'loan-details', component: LoanDetailsComponent},
+    {path: 'identity-details', component: IdentityDetailsComponent},
   ]
 },
   // {path: '/about-us', component: AboutUsComponent},
